ce179f553b97f8160520208cd607915a6dfcffe1
Joe Kopena <tjkopena@bellerophonmobile.com>
2014-11-03 12:04:06 -0500

    Documentation.

--- doc.go
+++ doc.go
@@ -3,17 +3,21 @@
 Package gobuildflags constructs command line flags and a file parser
 to manipulate a given structure.  It uses reflection to traverse a
 potentially hierarchical object of structs and maps and install
-handlers in Go's standard flag package.  Constructed flags have the
-form Foo.Bar... where Foo and Bar are keys of the map or exposed
-fields of the containing object.  The associated parser simply scans
-line by line and applies any such key/value pairs that it finds.
+handlers in Go's standard flag package.  Constructed parsers simply
+scan given documents line by line and apply any key/value pairs found.
+
+Constructed flags have the form Foo=..., Bar=...,Obj.Field=... and so
+on.  Maps and exposed fields of structs with primitive types are
+consumed, so in this case Foo and Bar might be map keys or public
+struct fields to a primitive.  Nested maps and structs are followed,
+producing dot-notation hierarchical keys such as Obj.Field.
 
 Primitive types understood by gobuildflags include bool, float64,
 int64, int, string, uint64, and uint.
 
 Primitive fields in the given object and sub-objects must be settable.
-Primitive types included in a map may be set, as well as fields in
-structs passed by pointer.
+In general this means structs should be passed in as pointers.  Maps
+may also be set directly.
 
 */
 package gobuildflags
