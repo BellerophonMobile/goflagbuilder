e664927ca52451da515a220056e6847d5589d043
Tom Wambold <tom5760@gmail.com>
2018-06-19 11:21:00 -0400

    Allow slices of basic types to be set by repeating flags.

--- buildflags.go
+++ buildflags.go
@@ -9,13 +9,11 @@ import (
 var Strict = false
 
 func populateMapFlags(flags FlagSet, parser *Parser, prefix string, mapval reflect.Value) error {
-
 	if prefix != "" {
 		prefix += "."
 	}
 
 	for _, keyval := range mapval.MapKeys() {
-
 		subprefix := prefix + keyval.String()
 		elementval := mapval.MapIndex(keyval)
 
@@ -39,8 +37,6 @@ func populateMapFlags(flags FlagSet, parser *Parser, prefix string, mapval refle
 			fallthrough
 
 		case reflect.Uint:
-			// Potentially get a usage string from a tag?
-
 			set := &mapvalue{subprefix, mapval, keyval, elementval}
 			flags.Var(set, subprefix, "")
 			parser.add(subprefix, set)
@@ -50,29 +46,29 @@ func populateMapFlags(flags FlagSet, parser *Parser, prefix string, mapval refle
 			if err != nil {
 				return err
 			}
-
 		}
-
 	}
 
 	return nil
-
 }
 
 func populateStructFlags(flags FlagSet, parser *Parser, prefix string, structval reflect.Value) error {
-
 	if prefix != "" {
 		prefix += "."
 	}
 
 	structtype := structval.Type()
 	for i := 0; i < structval.NumField(); i++ {
-
 		field := structtype.Field(i)
 		elementval := structval.Field(i)
 		subprefix := prefix + field.Name
 
-		switch elementval.Kind() {
+		kind := elementval.Kind()
+		if kind == reflect.Slice {
+			kind = elementval.Type().Elem().Kind()
+		}
+
+		switch kind {
 		case reflect.Bool:
 			fallthrough
 
@@ -95,9 +91,8 @@ func populateStructFlags(flags FlagSet, parser *Parser, prefix string, structval
 			if !elementval.CanSet() {
 				if Strict {
 					return fmt.Errorf("Value of type %s at %s cannot be set", elementval.Type().Name(), subprefix)
-				} else {
-					continue
 				}
+				continue
 			}
 
 			set := &flagvalue{subprefix, elementval}
@@ -110,28 +105,23 @@ func populateStructFlags(flags FlagSet, parser *Parser, prefix string, structval
 				return err
 			}
 		}
-
 	}
 
 	return nil
 }
 
 func recursePtrFlags(flags FlagSet, parser *Parser, prefix string, ptrval reflect.Value) error {
-
 	if ptrval.IsNil() {
 		if Strict {
 			return fmt.Errorf("Cannot build flags from nil pointer for prefix '%s'", prefix)
-		} else {
-			return nil
 		}
+		return nil
 	}
 
 	return recurseBuildFlags(flags, parser, prefix, ptrval.Elem())
-
 }
 
 func recurseBuildFlags(flags FlagSet, parser *Parser, prefix string, elementval reflect.Value) error {
-
 	switch elementval.Kind() {
 	case reflect.Map:
 		return populateMapFlags(flags, parser, prefix, elementval)
@@ -147,20 +137,15 @@ func recurseBuildFlags(flags FlagSet, parser *Parser, prefix string, elementval 
 	default:
 		if Strict {
 			return fmt.Errorf("Cannot build flags from type %v for prefix '%s'", elementval.Type(), prefix)
-		} else {
-			return nil
 		}
+		return nil
 	}
-
-	return nil
-
 }
 
 // Into populates the given flag set with hierarchical fields from the
 // given object.  It returns a Parser that may be used to read those
 // same flags from a configuration file.
 func Into(flags FlagSet, configuration interface{}) (*Parser, error) {
-
 	if configuration == nil {
 		return nil, errors.New("Cannot build flags from nil")
 	}
@@ -173,7 +158,6 @@ func Into(flags FlagSet, configuration interface{}) (*Parser, error) {
 	}
 
 	return parser, nil
-
 }
 
 // From populates the top-level default flags with hierarchical fields
--- flagvalue.go
+++ flagvalue.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 type flagvalue struct {
@@ -12,72 +13,92 @@ type flagvalue struct {
 }
 
 func (x *flagvalue) String() string {
-	return x.value.String()
+	if x.value.Kind() == reflect.Slice {
+		var b strings.Builder
+		b.WriteString("[")
+
+		for i := 0; i < x.value.Len(); i++ {
+			if i != 0 {
+				b.WriteString(", ")
+			}
+			b.WriteString(x.value.Index(i).String())
+		}
+
+		b.WriteString("]")
+		return b.String()
+	} else {
+		return x.value.String()
+	}
 }
 
 func (x *flagvalue) Set(str string) error {
-
 	if !x.value.IsValid() {
 		return fmt.Errorf("Flag variable value of type %s for %s is invalid", x.value.Type().Name(), x.name)
 	}
 
 	if !x.value.CanSet() {
-		return fmt.Errorf("Flag variable value of type %s for %s cannot be set", x.value.Type().Name(), x.name)
+		return fmt.Errorf("Flag variable value of type %s for %s cannot be set", x.value.Type(), x.name)
 	}
 
-	switch x.value.Kind() {
+	kind := x.value.Kind()
+	if kind == reflect.Slice {
+		kind = x.value.Type().Elem().Kind()
+	}
+
+	var argValue reflect.Value
+	var err error
+
+	switch kind {
 	case reflect.Bool:
-		b, err := strconv.ParseBool(str)
-		if err != nil {
-			return err
-		}
-		x.value.SetBool(b)
+		var b bool
+		b, err = strconv.ParseBool(str)
+		argValue = reflect.ValueOf(b)
 
 	case reflect.Float64:
-		f, err := strconv.ParseFloat(str, 64)
-		if err != nil {
-			return err
-		}
-		x.value.SetFloat(f)
+		var f float64
+		f, err = strconv.ParseFloat(str, 64)
+		argValue = reflect.ValueOf(f)
 
 	case reflect.Int64:
-		i, err := strconv.ParseInt(str, 0, 64)
-		if err != nil {
-			return err
-		}
-		x.value.SetInt(i)
+		var i int64
+		i, err = strconv.ParseInt(str, 0, 64)
+		argValue = reflect.ValueOf(i)
 
 	case reflect.Int:
-		i, err := strconv.ParseInt(str, 0, 0)
-		if err != nil {
-			return err
-		}
-		x.value.SetInt(i)
+		var i int64
+		i, err = strconv.ParseInt(str, 0, 0)
+		argValue = reflect.ValueOf(int(i))
 
 	case reflect.String:
-		x.value.SetString(str)
+		argValue = reflect.ValueOf(str)
 
 	case reflect.Uint64:
-		i, err := strconv.ParseUint(str, 0, 64)
-		if err != nil {
-			return err
-		}
-		x.value.SetUint(i)
+		var i uint64
+		i, err = strconv.ParseUint(str, 0, 64)
+		argValue = reflect.ValueOf(i)
 
 	case reflect.Uint:
-		i, err := strconv.ParseUint(str, 0, 0)
-		if err != nil {
-			return err
-		}
-		x.value.SetUint(i)
+		var i uint64
+		i, err = strconv.ParseUint(str, 0, 0)
+		argValue = reflect.ValueOf(uint(i))
 
 	default:
 		return fmt.Errorf("Unsupported flag field variable type %v kind %v for prefix %s", x.value.Type(), x.value.Kind(), x.name)
 	}
 
+	if err != nil {
+		return nil
+	}
+
+	if x.value.Kind() == reflect.Slice {
+		argValue = reflect.Append(x.value, argValue)
+	}
+
+	x.value.Set(argValue)
+
 	return nil
 }
 
-func (x *flagvalue) IsBool() bool {
+func (x *flagvalue) IsBoolFlag() bool {
 	return x.value.Kind() == reflect.Bool
 }
